fb09a15cea3b2dd44eef0b71319024e461d20f61
salmatador <salmatador@moonstub.com>
2016-01-12 18:16:04 -0800

    Update File

---
+++ app/src/main/java/com/moonstub/basicengine/screens/TestBed.java
@@ -0,0 +1,49 @@
+package com.moonstub.basicengine.screens;
+
+import com.moonstub.basicengine.framework.GameActivity;
+import com.moonstub.basicengine.framework.GameScreen;
+
+/**
+ * Created by Micah on 1/12/2016.
+ */
+public class TestBed extends GameScreen {
+
+    public TestBed(GameActivity game) {
+        super(game);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void draw(float delta) {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public boolean onBackPressed() {
+        return false;
+    }
+}
